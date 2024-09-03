26f709538160181591c9b05b48e73709c92d7a8c
Xavier Florensa <xavier.florensa.yahoo.com>
2024-09-03 16:42:02 +0200

    Code simplified for clarification

--- ProjectFiles/NetSolution/RuntimeNetLogic1.cs
+++ ProjectFiles/NetSolution/RuntimeNetLogic1.cs
@@ -20,17 +20,17 @@ public class RuntimeNetLogic1 : BaseNetLogic
     
     public override void Start()
     {
-
-       
-       
+    //Take a look at the end of RuntimeNetlogic that appears on Login Button Netlogic on
+    //UI/LoginForm/LoginFormComponents/Login/LoginButton/LoginButtonLogic.cs
+    //To see user access rights
+     
        var navigationPanel = Project.Current.Get<NavigationPanel>("UI/MainWindow/NavigationPanel1");
        
        //Disable Navigation Panel before any user is logged
        navigationPanel.Enabled = false;
        //navigationPanel.ChangePanelByTabIndex(2,navigationPanel.NodeId);
        Log.Info("NavigationPanelname:",navigationPanel.BrowseName.ToString());
-       
-               
+            
         // Mostrar el usuari logat
         var usuario = Session.User;
         var user = Session.User.BrowseName;
@@ -56,66 +56,8 @@ public class RuntimeNetLogic1 : BaseNetLogic
         // Insert code to be executed when the user-defined logic is stopped
         myTask1.Dispose();
     }
-    [ExportMethod]
-    
-    public void Update()
-    {
-       Actualitza();
-       Log.Info("Updating: ");
-    }
 
     
-    
-   
-    
-     private void Actualitza()
-    {
-       // Mostrar el usuari logat
-       var usuario = Session.User;
-       var user = Session.User.BrowseName;
-       var mylabel = Project.Current.Get<Label>("UI/MainWindow/Label2");
-       mylabel.Text = user.ToString();
-       var navigationPanel = Project.Current.Get<NavigationPanel>("UI/MainWindow/NavigationPanel1");
-       //Mostra el grup del usuari
-       var userGroups = usuario.Refs.GetObjects(FTOptix.Core.ReferenceTypes.HasGroup, false);
-       if (user== "Anonymous")
-       {
-        navigationPanel.Enabled = false;
-       }
-       foreach (var group in userGroups)
-       {
-            Log.Info("User Group: ",group.BrowseName);
-            var mylabel_group = Project.Current.Get<Label>("UI/MainWindow/Label4");
-            mylabel_group.Text = group.BrowseName.ToString();
-            //Make Navigation Panel visible if rigt usergroup is logged
-            var  panelPC = navigationPanel.Panels[1];
-            var panelTrends = navigationPanel.Panels[2];
-            var panelDatabase = navigationPanel.Panels[3];
-            if (group.BrowseName.ToString()=="Technician")
-            {
-                navigationPanel.Enabled = true;
-                panelPC.Enabled = false;
-                panelTrends.Enabled = true;
-                panelDatabase.Enabled = true;
-            }
-            else 
-            {
-                if (group.BrowseName.ToString()=="Administrator")
-                { 
-                navigationPanel.Enabled = true;
-                panelPC.Enabled = true;
-                panelTrends.Enabled = false;
-                panelDatabase.Enabled = true;
-                }
-            else 
-                {
-                navigationPanel.Enabled = false;
-                }
-            
-            }
-       
-       } 
-    }
     private void block_panel_on_logout()
     {
         var user = Session.User.BrowseName;
